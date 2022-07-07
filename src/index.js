e98bc4127076bc31417f0400e20ef0faa93bfbfb
unknown <sntakirutimana72@gmail.com>
2022-07-07 13:33:11 +0200

    Use helpers instead of vanilla code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import { $withId } from './helpers/dom_selectors';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot($withId('root'));
 
 root.render(
   <React.StrictMode>
